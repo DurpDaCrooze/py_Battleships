1512594d3e77e1b75be0605627cfce54be02dfa3
DurpDaCrooze <cassarluca@gmail.com>
2025-04-05 17:57:55 +0200

    Init!!!

--- app.py
+++ app.py
@@ -1,7 +1,25 @@
+# Author: Luca Cassart 👍 
+
 def displayTitle():
-    # Get the title element
-    with open('ASCII_ART/titleScreen.txt', 'r') as file:
-        title = file.read()
-        print(title)
+    # Fetch ASCII title element from file
+    try:
+        with open('ASCII_ART/titleScreen.txt', 'r') as file:
+            title = file.read()
+            print(title)
+    except FileNotFoundError:
+        print("Error: ASCII title file not found.")
+        exit(1)
+
+def main_menu():
+    uIn = -1
+    while not (uIn > 0 and uIn < 4):
+        # Display the main menu
+        print("---===Main Menu===---", 
+            "\n1. | Enter User Info", 
+            "\n2. | Start New Game",
+            "\n3. | Quit",)
+        uIn = int(input("Select a valid option:"))
+    return uIn;    
 
 displayTitle()
+print(main_menu())
